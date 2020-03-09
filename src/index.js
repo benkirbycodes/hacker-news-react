90efb5ac9770523e0f93de04c55983be28a818ac
benkirbycodes <benkirbycodes@gmail.com>
2020-03-09 12:16:11 -0600

    Setting Up

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Page extends React.Component {
+  render() {
+    return <h1>This is the page</h1>;
+  }
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
